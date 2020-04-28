0f55153759a243f8ff806ab2a86dd4c5fcd90a0f
root <penso.oren@gmail.com>
2020-04-28 06:06:26 +0000

    tas demo

---
+++ tas_demos/demomagic/0-cf-info.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+
+. ./demo-magic.sh
+
+clear
+
+DEMO_PROMPT="localadmin@ubuntu:~/ "
+
+
+#pe "./pkslogin.sh"
+pe "cf login"
+pe "cf target"
+pe "cf apps"
+#pe "pks create cluster demo -e demo.lab.local --plan small -n 1 --network-profile lb-medium"
+#pe "cat no-nat-namespace.yaml"
+#pe "kubectl create -f no-nat-namespace.yaml"
+
