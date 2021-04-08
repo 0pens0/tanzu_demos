9e6657aaa77d8a6b853873ae73a55301954e841a
root <penso.oren@gmail.com>
2021-04-08 17:59:12 +0000

    scripts

---
+++ scripts/kubeconfig-script/load-k8s-configs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# If there's already a kubeconfig file in ~/.kube/config it will import that too and all the contexts
+DEFAULT_KUBECONFIG_FILE="$HOME/.kube/config"
+if test -f "${DEFAULT_KUBECONFIG_FILE}"
+then
+  export KUBECONFIG="$DEFAULT_KUBECONFIG_FILE"
+fi
+# Your additional kubeconfig files should be inside ~/.kube/config-files
+ADD_KUBECONFIG_FILES="$HOME/.kube/config-files"
+mkdir -p "${ADD_KUBECONFIG_FILES}"
+OIFS="$IFS"
+IFS=$'\n'
+for kubeconfigFile in `find "${ADD_KUBECONFIG_FILES}" -type f -name "*.yml" -o -name "*.yaml"`
+do
+    export KUBECONFIG="$kubeconfigFile:$KUBECONFIG"
+done
+IFS="$OIFS"
